50707bee67a9d1f69bff427e4cd9489c8fd4bd6f
Sravani <117716430+SravaniPutturu@users.noreply.github.com>
2023-08-27 13:20:11 +0530

    Update UserInfo.jsx

--- src/Components/UserInfo.jsx
+++ src/Components/UserInfo.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '../firebaseConfig';
-
+import { auth } from '../firebaseConfig'; 
+import {IoArrowBackCircleSharp} from 'react-icons/io5';
+import { useNavigate } from 'react-router-dom';
 
 const UserInfo = ({totalTests}) => {
 
     const [user] = useAuthState(auth)//firebase hook
+    const navigate = useNavigate()
     return (
-        <div className="user-profile">
+       <div>
+       <div onClick={()=>navigate('/')}> <IoArrowBackCircleSharp size="3rem"/></div>
+         <div className="user-profile">
+            
             <div className="user">
                 <div className="picture">
                     <AccountCircleIcon style={{transform : 'scale(4)'}}/>
@@ -27,6 +32,7 @@ const UserInfo = ({totalTests}) => {
                 <span>Total test taken - {totalTests}</span>
             </div>
         </div>
+       </div>
     )
 }
 
