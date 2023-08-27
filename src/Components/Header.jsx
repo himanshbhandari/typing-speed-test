1db1982d8c3e61bfa2d0883b82617388c2815653
Sravani <117716430+SravaniPutturu@users.noreply.github.com>
2023-08-27 13:17:32 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import  AccountCircle  from './AccountCircle';
+import React from 'react';
+import { IoLogoOctocat } from 'react-icons/io'; // Corrected import path
+import AccountCircle from './AccountCircle';
+import KeyboardIcon from '@mui/icons-material/Keyboard';
+import { useNavigate } from 'react-router-dom';
+
+
 const Header = () => {
+  const navigate = useNavigate()
   return (
     <div className="header">
-        <div className="logo">
-            LOGO
-        </div>
-        <div className="user-icon">
-            <AccountCircle/>
-        </div>
+      <div style={{display:'flex',justifyContent:'center',alignItems:'center',gap:'.3rem'}}>
+      <span><IoLogoOctocat size="2.5em" /></span> {/* Adjust the size value as needed */}
+      <span className='logo'>TypeCat</span>
+      <span style={{padding:"1rem"}} onClick={()=>navigate('/')}><KeyboardIcon  size="2em"/></span>
+      </div>
+      <div className="user-icon">
+        <AccountCircle />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
