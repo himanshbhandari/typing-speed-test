f0eeca844cd767eeae957414a0484c07f100d650
Sravani <117716430+SravaniPutturu@users.noreply.github.com>
2023-08-27 17:05:07 +0530

    Update ThemeJSON.js

--- src/Utilities/ThemeJSON.js
+++ src/Utilities/ThemeJSON.js
@@ -1,6 +1,6 @@
 
 const darkTheme = {
-    label : "Dark",
+    label : "Darken-black",
     background : 'black',
     textColor : 'white',
     typeBoxText : 'grey',
@@ -8,49 +8,49 @@ const darkTheme = {
 
 }
 const greyTheme = {
-    label : "Grey",
+    label : "Coloured-Grey",
     background : 'lightgrey',
     textColor : 'black',
     typeBoxText: 'grey',
     graphColor : 'grey'
 }
 const purpleTheme = {
-    label : "Lightgrey",
+    label : "Coloured-lightgrey",
     background : '	#484848',
     textColor : '#FF9999',
     typeBoxText : '#FF9999',
     graphColor : 'grey'
 }
 const Pink = {
-    label : "Pink",
+    label : "Coloured-Pink",
     background :"black",
     textColor : "#CC9900",
     typeBoxText : '#CC9966 ',
     graphColor : '#CC99FF'
 }
 const Green = {
-    label : "Green",
+    label : "Coloured-Green",
     background :"#CCFFCC",
     textColor : '#003333 ',
     typeBoxText : "#666666",
     graphColor : '#CC99FF'
 }
 const Blue = {
-    label : "Blue",
+    label : "Coloured-Blue",
     background :"#33CCFF",
     textColor : "#282828",
     typeBoxText : '	#505050',
     graphColor : '#CC99FF'
 }
 const LightPurple = {
-    label : "Light-Purple",
+    label : "Coloured-LightPurple",
     background :"#9999FF",
     textColor : "#282828",
     typeBoxText : '	#505050',
     graphColor : '#CC99FF'
 }
 const lightTheme = {
-    label : 'Light',
+    label : 'Lighten-white',
     background : 'white',
     textColor : 'black',
     typeBoxText : '	#606060',
@@ -62,13 +62,13 @@ const lightTheme = {
 
 
 export const themeOptions = [
-    {label:'Dark',value:darkTheme},
-    {label:'Grey',value:greyTheme},
-    {label:'Purple',value:purpleTheme},
-    {label:'Pink',value:Pink},
-    {label:'Green',value:Green},
-    {label:'Blue',value:Blue},
-    {label:'Light',value:lightTheme},
-    {label:'Light-Purple',value:LightPurple}
+    {label:'Darken-black',value:darkTheme},
+    {label:'Coloured-Grey',value:greyTheme},
+  //  {label:'Coloured-Purple',value:purpleTheme},
+    {label:'Coloured-Pink',value:Pink},
+    {label:'Coloured-Green',value:Green},
+    {label:'Coloured-Blue',value:Blue},
+    {label:'Lighten-white',value:lightTheme},
+    {label:'Coloured-LightPurple',value:LightPurple}
 
 ]
