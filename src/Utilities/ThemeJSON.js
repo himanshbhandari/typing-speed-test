bd223a3b614c3b3e80cb236ca0ad7f320edeb85b
Sravani <117716430+SravaniPutturu@users.noreply.github.com>
2023-08-27 13:25:19 +0530

    Update ThemeJSON.js

--- src/Utilities/ThemeJSON.js
+++ src/Utilities/ThemeJSON.js
@@ -3,19 +3,59 @@ const darkTheme = {
     label : "Dark",
     background : 'black',
     textColor : 'white',
-    typeBoxText : 'black'
+    typeBoxText : 'grey',
+    graphColor : 'white'
+
 }
 const greyTheme = {
     label : "Grey",
     background : 'lightgrey',
-    textColor : 'grey',
-    typeBoxText: 'black',
+    textColor : 'black',
+    typeBoxText: 'grey',
+    graphColor : 'grey'
 }
 const purpleTheme = {
-    label : "Purple",
+    label : "Lightgrey",
     background : '	#484848',
     textColor : '#FF9999',
-    typeBoxText : '#FF9999'
+    typeBoxText : '#FF9999',
+    graphColor : 'grey'
+}
+const Pink = {
+    label : "Pink",
+    background :"black",
+    textColor : "#CC9900",
+    typeBoxText : '#CC9966 ',
+    graphColor : '#CC99FF'
+}
+const Green = {
+    label : "Green",
+    background :"#CCFFCC",
+    textColor : '#003333 ',
+    typeBoxText : "#666666",
+    graphColor : '#CC99FF'
+}
+const Blue = {
+    label : "Blue",
+    background :"#33CCFF",
+    textColor : "#282828",
+    typeBoxText : '	#505050',
+    graphColor : '#CC99FF'
+}
+const LightPurple = {
+    label : "Light-Purple",
+    background :"#9999FF",
+    textColor : "#282828",
+    typeBoxText : '	#505050',
+    graphColor : '#CC99FF'
+}
+const lightTheme = {
+    label : 'Light',
+    background : 'white',
+    textColor : 'black',
+    typeBoxText : '	#606060',
+    graphColor : 'black'
+
 }
 
 
@@ -24,5 +64,11 @@ const purpleTheme = {
 export const themeOptions = [
     {label:'Dark',value:darkTheme},
     {label:'Grey',value:greyTheme},
-    {label:'Purple',value:purpleTheme}
+    {label:'Purple',value:purpleTheme},
+    {label:'Pink',value:Pink},
+    {label:'Green',value:Green},
+    {label:'Blue',value:Blue},
+    {label:'Light',value:lightTheme},
+    {label:'Light-Purple',value:LightPurple}
+
 ]
